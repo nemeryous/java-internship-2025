a1e0f48eee99cefd0e5018d80ee5f278d180776a
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-25 13:58:22 +0700

    tesst

--- src/main/java/basic/Exercise02.java
+++ src/main/java/basic/Exercise02.java
@@ -2,6 +2,7 @@ package basic;
 
 public class Exercise02 {
     public int sumDigits(int number) {
+        System.out.println("Test");
         return number > 0 ? number % 10 + sumDigits(number / 10) : 0;
     }
 }
