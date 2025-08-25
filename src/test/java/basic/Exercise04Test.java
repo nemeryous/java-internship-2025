6d3c0c86f7c78262fd4a28ee02e4e6777e129e1b
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-25 15:24:21 +0700

    fix java 9 List.of replace by Arrays.asList

--- src/test/java/basic/Exercise04Test.java
+++ src/test/java/basic/Exercise04Test.java
@@ -2,20 +2,19 @@ package basic;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 public class Exercise04Test {
 
-    
     @Test
     void testFibonacciList() {
-        assertEquals(Exercise04.fibonacciList(5), List.of(1, 1, 2, 3, 5));
-        assertEquals(Exercise04.fibonacciList(10), List.of(1, 1, 2, 3, 5, 8));
-        assertEquals(Exercise04.fibonacciList(1), List.of(1, 1));
-        assertEquals(Exercise04.fibonacciList(0), List.of());
-        assertEquals(Exercise04.fibonacciList(-5), List.of());
+        assertEquals(Exercise04.fibonacciList(5), Arrays.asList(1, 1, 2, 3, 5));
+        assertEquals(Exercise04.fibonacciList(10), Arrays.asList(1, 1, 2, 3, 5, 8));
+        assertEquals(Exercise04.fibonacciList(1), Arrays.asList(1, 1));
+        assertEquals(Exercise04.fibonacciList(0), Arrays.asList());
+        assertEquals(Exercise04.fibonacciList(-5), Arrays.asList());
     }
 
 }
