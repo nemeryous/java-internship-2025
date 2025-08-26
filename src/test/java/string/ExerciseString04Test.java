bc63284667fedfd98fb559973e8a11cb1c5835c8
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-26 23:40:03 +0700

    rename test method

--- src/test/java/string/ExerciseString04Test.java
+++ src/test/java/string/ExerciseString04Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class ExerciseString04Test {
 
     @Test
-    void testCompressString() {
+    void shouldCompressString_OK() {
         assertEquals("abc4e6fd", ExerciseString04.compressString("abcccceeeeeefd"));
         assertEquals("ab13c", ExerciseString04.compressString("abbbbbbbbbbbbbc"));
         assertEquals("a", ExerciseString04.compressString("a"));
