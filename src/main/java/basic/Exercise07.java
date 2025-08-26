6270be49b201cfecca252c4ec55a7e141c45e965
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-26 10:26:15 +0700

    Remove redundant

--- src/main/java/basic/Exercise07.java
+++
@@ -1,9 +0,0 @@
-package basic;
-
-public class Exercise07 {
-    public static int reverseBinary(int n) {
-        String binary = Integer.toBinaryString(n);
-        String reversed = new StringBuilder(binary).reverse().toString();
-        return Integer.parseInt(reversed, 2);
-    }
-}
