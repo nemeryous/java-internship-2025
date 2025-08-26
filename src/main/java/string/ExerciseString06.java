0265d01d47bea3485129f1c927e2f9076e3e88d2
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-26 23:43:29 +0700

    add string/exercise06

---
+++ src/main/java/string/ExerciseString06.java
@@ -0,0 +1,35 @@
+package string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExerciseString06 {
+    
+    public static boolean isValidBarcode(final String str) {
+        if (str == null || str.length() != 13) {
+            return false;
+        }
+
+        final List<Integer> nums = new ArrayList<>();
+
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+
+            nums.add(Character.getNumericValue(c));
+        }
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (i % 2 == 0) {
+                nums.set(i, nums.get(i) * 1);
+            } else {
+                nums.set(i, nums.get(i) * 3);
+            }
+        }
+
+        int sum = nums.stream().mapToInt(Integer::intValue).sum();
+
+        return sum % 10 == 0;
+    }
+}
