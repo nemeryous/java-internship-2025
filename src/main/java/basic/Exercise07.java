c70958734f7928e83c465ef7e4b03c4be6e1a9cb
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-26 14:15:39 +0700

    add final keyword

--- src/main/java/basic/Exercise07.java
+++ src/main/java/basic/Exercise07.java
@@ -1,7 +1,7 @@
 package basic;
 
 public class Exercise07 {
-    public static int reverseBinary(int n) {
+    public static int reverseBinary(final int n) {
         final String binary = Integer.toBinaryString(n);
         final String reversed = new StringBuilder(binary).reverse().toString();
         return Integer.parseInt(reversed, 2);
