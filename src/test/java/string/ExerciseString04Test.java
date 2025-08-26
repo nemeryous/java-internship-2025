f2a7173a7170d770d3d700c6518c7dd9d0e64cce
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-26 23:35:21 +0700

    add string/exercise04

---
+++ src/test/java/string/ExerciseString04Test.java
@@ -0,0 +1,20 @@
+package string;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ExerciseString04Test {
+
+    @Test
+    void testCompressString() {
+        assertEquals("abc4e6fd", ExerciseString04.compressString("abcccceeeeeefd"));
+        assertEquals("ab13c", ExerciseString04.compressString("abbbbbbbbbbbbbc"));
+        assertEquals("a", ExerciseString04.compressString("a"));
+        assertEquals("a2b2c2", ExerciseString04.compressString("aabbcc"));
+        assertEquals("abc", ExerciseString04.compressString("abc"));
+        assertEquals("", ExerciseString04.compressString(""));
+        assertEquals("A2B2C2", ExerciseString04.compressString("AABBCC"));
+        assertEquals("aA", ExerciseString04.compressString("aA"));
+    }
+}
