bfece5eeca4459b70d354e1e2edcfba136483176
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-26 22:56:41 +0700

    add string/exercise01

--- src/main/java/string/ExerciseString01.java
+++ src/main/java/string/ExerciseString01.java
@@ -1,28 +1,37 @@
 package string;
 
 public class ExerciseString01 {
-     public static boolean isPalindrome(String s) {
-         int i = 0;
+
+  public static boolean isPalindrome(final String s) {
+
+    int i = 0;
     int j = s.length() - 1;
+
     while (i < j) {
       while (i < j && !alphaNum(s.charAt(i))) {
         i++;
       }
+
       while (i < j && !alphaNum(s.charAt(j))) {
         j--;
       }
+
       if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
         return false;
       }
+
       i++;
       j--;
+    }
 
-    }
     return true;
-    }
-    public static boolean alphaNum(char c) {
+  }
+
+  public static boolean alphaNum(char c) {
+
     return (c >= 'A' && c <= 'Z' ||
         c >= 'a' && c <= 'z' ||
         c >= '0' && c <= '9');
+        
   }
 }
--- src/test/java/string/ExerciseString01Test.java
+++ src/test/java/string/ExerciseString01Test.java
@@ -6,8 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class ExerciseString01Test {
+
     @Test
-    void testIsPalindrome() {
+    void shouldIsPalindrome_OK() {
         assertTrue(ExerciseString01.isPalindrome("racecar"));
         assertTrue(ExerciseString01.isPalindrome("AmanaplanacanalPanama"));
         assertFalse(ExerciseString01.isPalindrome("hello"));
