f09569b93c8e8fe72f076b3dc9c0bc52e87dea81
nemeryous <115894128+nemeryous@users.noreply.github.com>
2025-08-26 14:14:12 +0700

    Update src/main/java/basic/Exercise07.java
    Co-authored-by: Mẫn <vanman.lqd@gmail.com>

--- src/main/java/basic/Exercise07.java
+++ src/main/java/basic/Exercise07.java
@@ -2,7 +2,7 @@ package basic;
 
 public class Exercise07 {
     public static int reverseBinary(int n) {
-        String binary = Integer.toBinaryString(n);
+        final String binary = Integer.toBinaryString(n);
         String reversed = new StringBuilder(binary).reverse().toString();
         return Integer.parseInt(reversed, 2);
     }
