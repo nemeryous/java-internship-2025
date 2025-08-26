7a7907396fd948e83dfee00bc92d3eb6a6463949
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-26 21:11:00 +0700

    fix coding convention

--- src/main/java/basic/Exercise01.java
+++ src/main/java/basic/Exercise01.java
@@ -3,16 +3,23 @@ package basic;
 public class Exercise01 {
     public static int findGCD(int a, int b) {
         while (b != 0) {
+
             int temp = b;
             b = a % b;
             a = temp;
+
         }
+
         return a;
     }
 
     public static int findLCM(int a, int b) {
-        if (a == 0 || b == 0)
+        if (a == 0 || b == 0) {
+
             return 0;
+
+        }
+
         return a * b / findGCD(a, b);
     }
 }
--- src/test/java/basic/Exercise01Test.java
+++ src/test/java/basic/Exercise01Test.java
@@ -5,18 +5,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class Exercise01Test {
+    
     @Test
-    void testGcd() {
+    void shouldFindGcd_OK() {
+
         assertEquals(6, Exercise01.findGCD(12, 18));
         assertEquals(1, Exercise01.findGCD(7, 5));
         assertEquals(0, Exercise01.findGCD(0, 0));
+
     }
 
     @Test
-    void testLcm() {
+    void shouldFindLcm_OK() {
+
         assertEquals(36, Exercise01.findLCM(12, 18));
         assertEquals(35, Exercise01.findLCM(7, 5));
         assertEquals(0, Exercise01.findLCM(0, 0));
+
     }
 
 }
