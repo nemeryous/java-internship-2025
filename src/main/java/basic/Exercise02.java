d76c2507970f4bcb526404c6b565394993a90a94
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-26 10:21:30 +0700

    Remove redundant

--- src/main/java/basic/Exercise02.java
+++
@@ -1,8 +0,0 @@
-package basic;
-
-public class Exercise02 {
-    public int sumDigits(int number) {
-        System.out.println("Test");
-        return number > 0 ? number % 10 + sumDigits(number / 10) : 0;
-    }
-}
