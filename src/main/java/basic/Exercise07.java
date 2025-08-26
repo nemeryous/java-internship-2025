5208e10100205780a738eba4ba4373bab98c68bf
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-26 22:52:24 +0700

    use for-loop instead of using built-in method

--- src/main/java/basic/Exercise07.java
+++ src/main/java/basic/Exercise07.java
@@ -2,8 +2,26 @@ package basic;
 
 public class Exercise07 {
     public static int reverseBinary(final int n) {
-        final String binary = Integer.toBinaryString(n);
-        final String reversed = new StringBuilder(binary).reverse().toString();
-        return Integer.parseInt(reversed, 2);
+
+        if (n == 0) {
+            return 0;
+        }
+
+        StringBuilder binary = new StringBuilder();
+        int temp = n;
+
+        while (temp > 0) {
+            binary.append(temp % 2);
+            temp /= 2;
+        }
+        
+        int newNumber = 0;
+        for (int i = binary.length() - 1; i >= 0; i--) {
+            int bit = binary.charAt(i) - '0';
+            System.out.println(bit);
+            newNumber += bit * (int) Math.pow(2, binary.length() - 1 - i);
+        }
+
+        return newNumber;
     }
 }
--- src/test/java/basic/Exercise07Test.java
+++ src/test/java/basic/Exercise07Test.java
@@ -5,10 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class Exercise07Test {
+
     @Test
-    void testReverseBinary() {
+    void shouldReverseBinary_OK() {
         assertEquals(Exercise07.reverseBinary(5), 5);
         assertEquals(Exercise07.reverseBinary(10), 5);
         assertEquals(Exercise07.reverseBinary(15), 15);
+        assertEquals(Exercise07.reverseBinary(23), 29);
+        assertEquals(Exercise07.reverseBinary(8), 1);
+        assertEquals(Exercise07.reverseBinary(0), 0);
     }
 }
