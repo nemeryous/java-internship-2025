c79e168ee2c12bf1b7c65962b8242adedae84c09
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-26 23:38:24 +0700

    add string/exercise05

---
+++ src/main/java/string/ExerciseString05.java
@@ -0,0 +1,39 @@
+package string;
+
+public class ExerciseString05 {
+
+    public static String decompressString(final String input) {
+        
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+
+        final StringBuilder result = new StringBuilder();
+        int i = 0;
+
+        while (i < input.length()) {
+            char c = input.charAt(i);
+            int j = i + 1;
+            final StringBuilder num = new StringBuilder();
+
+            while (j < input.length() && Character.isDigit(input.charAt(j))) {
+                num.append(input.charAt(j));
+                j++;
+            }
+
+            int repeat = 1;
+
+            if (num.length() > 0) {
+                repeat = Integer.parseInt(num.toString());
+            }
+
+            for (int k = 0; k < repeat; k++) {
+                result.append(c);
+            }
+
+            i = j;
+        }
+
+        return result.toString();
+    }
+}
