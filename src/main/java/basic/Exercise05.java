ad23e1d63ec0996583bdd70b809a4eb4e9688cd1
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-26 21:24:20 +0700

    fix coding convention

--- src/main/java/basic/Exercise05.java
+++ src/main/java/basic/Exercise05.java
@@ -2,12 +2,14 @@ package basic;
 
 public class Exercise05 {
     public static long factorial(final int n) {
-        if (n < 0)
-            return 1;
+
         long result = 1;
+
         for (int i = 2; i <= n; i++) {
             result *= i;
         }
+
         return result;
+
     }
 }
--- src/test/java/basic/Exercise05Test.java
+++ src/test/java/basic/Exercise05Test.java
@@ -5,10 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class Exercise05Test {
+
     @Test
-    void testFactorial() {
+    void shouldTestFactorial_OK() {
+
         assertEquals(Exercise05.factorial(5), 120);
         assertEquals(Exercise05.factorial(0), 1);
         assertEquals(Exercise05.factorial(-5), 1);
+
     }
 }
