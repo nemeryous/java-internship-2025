4ee13b1ca0b629a2c767d70089f89fd4796d43df
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-26 21:48:55 +0700

    take months variable out of method, add private static final for var months

--- src/main/java/basic/Exercise06.java
+++ src/main/java/basic/Exercise06.java
@@ -1,12 +1,13 @@
 package basic;
 
 public class Exercise06 {
-    public static String getMonthName(final int month) {
 
-        String[] months = {
-                "Tháng Một", "Tháng Hai", "Tháng Ba", "Tháng Tư", "Tháng Năm", "Tháng Sáu",
-                "Tháng Bảy", "Tháng Tám", "Tháng Chín", "Tháng Mười", "Tháng Mười Một", "Tháng Mười Hai"
-        };
+    private static final String[] months = {
+            "Tháng Một", "Tháng Hai", "Tháng Ba", "Tháng Tư", "Tháng Năm", "Tháng Sáu",
+            "Tháng Bảy", "Tháng Tám", "Tháng Chín", "Tháng Mười", "Tháng Mười Một", "Tháng Mười Hai"
+    };
+
+    public static String getMonthName(final int month) {
 
         if (month >= 1 && month <= 12) {
             return months[month - 1];
