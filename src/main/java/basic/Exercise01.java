b544ebfe30c7ca17ba3b0c6d83e4c1045f36ed6b
nemeryous <nammp.22it@vku.udn.vn>
2025-08-26 15:52:18 +0700

    123

--- src/main/java/basic/Exercise01.java
+++ src/main/java/basic/Exercise01.java
@@ -11,7 +11,8 @@ public class Exercise01 {
     }
 
     public static int lcm(int a, int b) {
-        if (a == 0 || b == 0) return 0;
+        if (a == 0 || b == 0)
+            return 0;
         return a * b / gcd(a, b);
     }
 }
