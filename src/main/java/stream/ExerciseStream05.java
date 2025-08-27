4af3f68add35a32472067e8189de4d0a2637be70
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-27 17:29:15 +0700

    add stream/exercise05

---
+++ src/main/java/stream/ExerciseStream05.java
@@ -0,0 +1,14 @@
+package stream;
+
+import java.util.Arrays;
+
+public class ExerciseStream05 {
+
+    public static int sumOfLongestNumbers(final int[] arr) {
+        int maxLength = Arrays.stream(arr).boxed().map(String::valueOf).mapToInt(String::length).max().orElse(0);
+
+        return Arrays.stream(arr)
+                .filter(n -> String.valueOf(n).length() == maxLength)
+                .sum();
+    }
+}
