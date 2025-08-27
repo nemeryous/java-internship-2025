bc950e39e3fbbc910e03d7e4f89ad750f90f2072
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-27 13:50:41 +0700

    add array/exercise01

---
+++ src/test/java/array/ExerciseArray01Test.java
@@ -0,0 +1,16 @@
+package array;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ExerciseArray01Test {
+
+    @Test
+    void shouldSumEvenMinusOdd() {
+        assertEquals(2, ExerciseArray01.sumEvenMinusOdd(new int[] { 2, 4, 1, 3 }));
+        assertEquals(2, ExerciseArray01.sumEvenMinusOdd(new int[] { 1, 2, 3, 4 }));
+        assertEquals(-9, ExerciseArray01.sumEvenMinusOdd(new int[] { 1, 3, 5 }));
+        assertEquals(12, ExerciseArray01.sumEvenMinusOdd(new int[] { 2, 4, 6 }));
+    }
+}
