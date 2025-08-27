c5cae27b58bec5edb03adff1e976df6d7d883b45
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-27 14:00:42 +0700

    rename test method

--- src/test/java/array/ExerciseArray01Test.java
+++ src/test/java/array/ExerciseArray01Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class ExerciseArray01Test {
 
     @Test
-    void shouldSumEvenMinusOdd() {
+    void shouldSumEvenMinusOdd_OK() {
         assertEquals(2, ExerciseArray01.sumEvenMinusOdd(new int[] { 2, 4, 1, 3 }));
         assertEquals(2, ExerciseArray01.sumEvenMinusOdd(new int[] { 1, 2, 3, 4 }));
         assertEquals(-9, ExerciseArray01.sumEvenMinusOdd(new int[] { 1, 3, 5 }));
