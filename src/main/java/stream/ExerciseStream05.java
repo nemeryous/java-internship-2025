d1833db3d3055d827e22abd59c9d5f6cafefc633
nemeryous <nammp.22it@vku.udn.vn>
2025-08-27 21:30:32 +0700

    optimize code

--- src/main/java/stream/ExerciseStream05.java
+++ src/main/java/stream/ExerciseStream05.java
@@ -5,7 +5,11 @@ import java.util.Arrays;
 public class ExerciseStream05 {
 
     public static int sumOfLongestNumbers(final int[] arr) {
-        int maxLength = Arrays.stream(arr).boxed().map(String::valueOf).mapToInt(String::length).max().orElse(0);
+        int maxLength = Arrays.stream(arr)
+                .mapToObj(String::valueOf)
+                .mapToInt(String::length)
+                .max()
+                .orElse(0);
 
         return Arrays.stream(arr)
                 .filter(n -> String.valueOf(n).length() == maxLength)
