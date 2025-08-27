f6ae0d83706e3c848aec8c2c2d929ca906fdbcdf
nemeryous <nammp.22it@vku.udn.vn>
2025-08-27 21:20:28 +0700

    change digit var defination

--- src/main/java/string/ExerciseString06.java
+++ src/main/java/string/ExerciseString06.java
@@ -10,8 +10,8 @@ public class ExerciseString06 {
         int sum = 0;
 
         for (int i = 0; i < str.length(); i++) {
-            final int digit = Character.getNumericValue(str.charAt(i));
-            
+            final int digit = str.charAt(i) - '0';
+
             if ((i + 1) % 2 == 0) {
                 sum += digit * 3;
             } else {
