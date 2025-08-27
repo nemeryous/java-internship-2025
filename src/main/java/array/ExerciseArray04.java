1790f7d84ac2c8f4ae97f0558de44bd301ecfcc9
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-27 16:19:43 +0700

    optimize code

--- src/main/java/array/ExerciseArray04.java
+++ src/main/java/array/ExerciseArray04.java
@@ -1,34 +1,33 @@
 package array;
 
+import java.util.Arrays;
+
 public class ExerciseArray04 {
-    
+
     public static int[] mergeTwoSortedArray(final int[] arr1, final int[] arr2) {
 
-        final int[] merge = new int[arr1.length + arr2.length];
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        final boolean isAsc = (arr1.length > 1 && arr1[0] < arr1[1]) || (arr2.length > 1 && arr2[0] < arr2[1]);
-
-        while (i < arr1.length && j < arr2.length) {
-            if (isAsc) {
-                if (arr1[i] < arr2[j]) merge[k++] = arr1[i++];
-                else merge[k++] = arr2[j++];
-            } else {
-                if (arr1[i] > arr2[j]) merge[k++] = arr1[i++];
-                else merge[k++] = arr2[j++];
-            }
+        int i = arr1.length - 1;
+        int j = arr2.length - 1;
+        final int[] merge = new int[i + j + 2];
+        int k = merge.length - 1;
+        
+        while (j >= 0) {
+            if (i >= 0 && arr1[i] > arr2[j]) merge[k--] = arr1[i--];
+            else merge[k--] = arr2[j--];
         }
 
-        while (i < arr1.length) {
-            merge[k++] = arr1[i++];
-        }
-
-        while (j < arr2.length) {
-            merge[k++] = arr2[j++];
+        while (i >= 0) {
+            merge[k--] = arr1[i--];
         }
 
         return merge;
 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(mergeTwoSortedArray(new int[] { 1, 3, 5 }, new int[] { 2, 4, 6, 7 })));
+        System.out.println(Arrays.toString(mergeTwoSortedArray(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 })));
+        System.out.println(Arrays.toString(mergeTwoSortedArray(new int[] { 1, 2, 3 }, new int[] { 2, 4, 5 })));
+        System.out.println(Arrays.toString(mergeTwoSortedArray(new int[] { 1, 2 }, new int[] { 3, 4 })));
     }
 }
--- src/test/java/array/ExerciseArray04Test.java
+++ src/test/java/array/ExerciseArray04Test.java
@@ -9,8 +9,8 @@ public class ExerciseArray04Test {
     @Test
     void shouldMergeTwoSortedArray_OK() {
         assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6, 7 }, ExerciseArray04.mergeTwoSortedArray(new int[] { 1, 3, 5 }, new int[] { 2, 4, 6, 7 }));
-        assertArrayEquals(new int[] { 1, 2, 3 }, ExerciseArray04.mergeTwoSortedArray(new int[] { 1, 2, 3 }, new int[] {}));
-        assertArrayEquals(new int[] {}, ExerciseArray04.mergeTwoSortedArray(new int[] {}, new int[] {}));
-        assertArrayEquals(new int[] { 7, 6, 5, 4, 3, 2, 1 }, ExerciseArray04.mergeTwoSortedArray(new int[] { 5, 3, 1 }, new int[] { 7, 6, 4, 2 }));
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6 }, ExerciseArray04.mergeTwoSortedArray(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }));
+        assertArrayEquals(new int[] { 1, 2, 2, 3, 4, 5 }, ExerciseArray04.mergeTwoSortedArray(new int[] { 1, 2, 3 }, new int[] { 2, 4, 5 }));
+        assertArrayEquals(new int[] { 1, 2, 3, 4 }, ExerciseArray04.mergeTwoSortedArray(new int[] { 1, 2 }, new int[] { 3, 4 }));
     }
 }
