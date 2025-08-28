1867a3111a081983bdcb62a71f4d0b626fc8bebd
Mai Phương Nam <nammp.22it@vku.udn.vn>
2025-08-28 16:10:10 +0700

    optimize code

--- src/main/java/basic/Exercise07.java
+++ src/main/java/basic/Exercise07.java
@@ -2,20 +2,19 @@ package basic;
 
 public class Exercise07 {
     public static int reverseBinary(final int n) {
-
         if (n == 0) {
             return 0;
         }
+
         int temp = n;
         int newNumber = 0;
-
 
         while (temp > 0) {
             final int bit = temp & 1;
             newNumber = (newNumber << 1) | bit;
             temp >>= 1;
         }
-        
+
         return newNumber;
     }
 }
